94de0fa8a0e31eed5234e705293495692994525d
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-14 21:33:02 +0000

    index added to the list

--- directory.rb
+++ directory.rb
@@ -19,30 +19,15 @@ def input_students
   students
 end
 
-=begin
-students = [
-    {name: "Dr. Hannibal Lechter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "The Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november},
-  ]
-=end  
-
 def print_header
   puts "The Students of the Villains Academmy"
   puts "-----------"
 end
 
 def prints(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index| #use each_with_index
+    index1 = index + 1 # so the list starts from 1 not from 0.
+    puts "#{index1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
