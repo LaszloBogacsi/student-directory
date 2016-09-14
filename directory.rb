b1d517599d01bfd90a37a7288abd0018aa5993d8
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-14 21:21:54 +0000

    user input method added

--- directory.rb
+++ directory.rb
@@ -1,4 +1,25 @@
 #Let's put all students into an array
+
+def input_students
+  puts "please enter the name of the students"
+  puts "To finish, just hit enter twice."
+  #create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
+=begin
 students = [
     {name: "Dr. Hannibal Lechter", cohort: :november},
     {name: "Darth Vader", cohort: :november},
@@ -12,7 +33,7 @@ students = [
     {name: "Joffrey Baratheon", cohort: :november},
     {name: "Norman Bates", cohort: :november},
   ]
-  
+=end  
 
 def print_header
   puts "The Students of the Villains Academmy"
@@ -31,6 +52,8 @@ print "Overall, we have #{students.count} great students"
 end
 
 #nothing happens until we call the methods
+students = input_students
+
 print_header
 prints(students)
 print_footer(students)
