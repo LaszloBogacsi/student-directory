c44ca57505c530c67883f9403127657102d8632c
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-14 22:09:21 +0000

    only taking names with specific letter

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 #Let's put all students into an array
-
+@first_letter = 'D' # we only take names starting with this letter
 def input_students
   puts "please enter the name of the students"
   puts "To finish, just hit enter twice."
@@ -10,10 +10,12 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
+      students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
+    
     #get another name from the user
     name = gets.chomp
+  
   end
   #return the array of students
   students
@@ -26,8 +28,10 @@ end
 
 def prints(students)
   students.each_with_index do |student, index| #use each_with_index
-    index1 = index + 1 # so the list starts from 1 not from 0.
-    puts "#{index1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].index(@first_letter) == 0 # print only the names starting with a specific letter
+      indexplusone = index + 1 # so the list starts from 1 not from 0.
+      puts "#{indexplusone}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
