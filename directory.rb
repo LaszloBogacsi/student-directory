51da10f256277b236ba322f4caeae3cca0ba0542
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2016-09-17 12:40:05 +0000

    more refactoring from excercise #10.

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
 #Let's put all students into an array
 @first_letter = 'D' # we only take names starting with this letter
+@students = []
 students_list = [  #hardcoded list only to test the filtering method
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :january},
@@ -17,8 +18,6 @@ students_list = [  #hardcoded list only to test the filtering method
 def input_students
   puts "please enter the name of the students"
   puts "To finish, just hit enter twice."
-  #create an empty array
-  students = []
   #get the first name
   name = gets.delete "\n"
   if name != ""
@@ -28,14 +27,14 @@ def input_students
   while !name.empty? do
     #add the student hash to the array
       
-      students << {name: name, cohort: cohort, hobbies: :fishing, height: :'6ft'}
+      @students << {name: name, cohort: cohort, hobbies: :fishing, height: :'6ft'}
     if cohort.empty?
-      students[students.count - 1][:cohort] = "December" # if the cohort is empty the default value will be december
+      @students[@students.count - 1][:cohort] = "December" # if the cohort is empty the default value will be december
     end
-    if students.count == 1
+    if @students.count == 1
       puts "Now we have 1 student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     #get another name from the user
     name = gets.chomp
@@ -45,8 +44,6 @@ def input_students
     puts "No user input"
     exit
   end
-      #return the array of students
-  students
 end
 
 def print_header
@@ -57,80 +54,77 @@ end
 def filtered_cohort(students_list)
  puts "Type the cohort filter"
  cohort_filter = gets.chomp.downcase
-  students.map do |cohort| 
+  @students.map do |cohort| 
     if cohort[:cohort] == cohort_filter.to_sym
         puts "Name: #{cohort[:name]} Cohort: #{cohort[:cohort]}"
     end
   end
 end
 
-def prints(students)
+def print_student_list
   
-  list_length = students.count #length of the student list
+  list_length = @students.count #length of the student list
   i = 0 # loop counter
   while i < list_length #loop while the counter is less than the length of the list
-    if students[i][:name].length <= 12
-     if students[i][:name].upcase.index(@first_letter) == 0 # print only the names starting with a specific letter
+    if @students[i][:name].length <= 12
+     if @students[i][:name].upcase.index(@first_letter) == 0 # print only the names starting with a specific letter
         indexplusone = i + 1 # so the list starts from 1 not from 0.
-        puts "#{indexplusone}. #{students[i][:name]} (#{students[i][:cohort]} cohort), hobby: #{students[i][:hobbies]}, height: #{students[i][:height]}".center(80)
+        puts "#{indexplusone}. #{@students[i][:name]} (#{@students[i][:cohort]} cohort), hobby: #{@students[i][:hobbies]}, height: #{@students[i][:height]}".center(80)
      end
     end
     i = i+ 1 #increment the counter
   end
 end
 
-def print_footer(students)
+def print_footer
   #We print out the total number of students
-  if students.count == 1
+  if @students.count == 1
     print "Overall, we have 1 student"
     puts ""
   else
-    print "Overall, we have #{students.count} great students"
+    print "Overall, we have #{@students.count} great students"
     puts""
   end
 end
 
 def interactive_menu
-  students = []
-  loop do
-    
-    #print the menu and ask the user what to do
+  loop do  
+    print_menu
+    process(gets.chomp)
+    end
+end
+
+def print_menu
+   #print the menu and ask the user what to do
     puts "1. Input the students"
     puts "2. Show the students"
     puts "9. Exit"
-    
-    #read the input and save it into a variable
-    selection = gets.chomp
-    
-    #do what the user asked
-    case selection
+end
+
+def show_students
+   #show the students
+        print_header
+        print_student_list
+        print_footer
+end
+
+def process(selection)
+  case selection
       when "1"
         #input the students
-        students = input_students
-        
+        input_students
       when "2"
-        #show the students
-        print_header
-        prints(students)
-        print_footer(students)
+        show_students
       when "9"
         exit #this will cause the program to terminate  
       else
         puts "I don't know what you ment, try again"
       end
     end
-    
-end
-
 
 
 
 #nothing happens until we call the methods
 
 interactive_menu
-#students = input_students #to test the filter I'm usig the hardcoded list of students instead of the user input method 
-
-#print_header
-#filtered_cohort(students) #print out the cohort filtered list
-#prints(students) 
-#print_footer(students)
+#filtered_cohort #this will show the cohort filtered list
